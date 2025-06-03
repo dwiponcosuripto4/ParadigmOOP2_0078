4260109c3a9a435110e283de126b211bfe54c33f
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2025-06-03 15:02:40 +0700

    member orang

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -3,7 +3,11 @@ using namespace std;
 
 class orang
 {
-private:
-    /* data */
 public:
+    int umur;
+    orang(int pUmur) : umur(pUmur)
+    {
+        cout << "orang dibuat dengan umur" << umur << "\n"
+             << endl;
+    }
 };
