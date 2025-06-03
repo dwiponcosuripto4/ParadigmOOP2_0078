2d15d76c70560bfd831d4c164df52c352c44523d
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2025-06-03 15:20:39 +0700

    class budi

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -17,7 +17,32 @@ class pekerja : public orang
 public:
     pekerja(int pUmur) : orang(pUmur)
     {
-        cout << "orang dibuat dengan umur" << umur << "\n"
+        cout << "pekerja dibuat\n"
              << endl;
     }
 };
+
+class pelajar : public orang
+{
+public:
+    pelajar(int pUmur) : orang(pUmur)
+    {
+        cout << "pelajar dibuat\n"
+             << endl;
+    }
+};
+
+class budi : public pekerja, public pelajar
+{
+public:
+    budi(int pUmur) : pekerja(pUmur), pelajar(pUmur)
+    {
+        cout << "budi dibuat\n"
+             << endl;
+    }
+};
+
+int main()
+{
+    budi a(12);
+}
