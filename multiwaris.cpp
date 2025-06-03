7ab75e9ba53ce21ac06e288a693084f40d230e5f
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2025-06-03 15:10:40 +0700

    main program

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -31,3 +31,20 @@ public:
              << endl;
     }
 };
+
+class budi : public pekerja, public pelajar
+{
+public:
+    budi(int pUmur) : pekerja(pUmur), pelajar(pUmur), orang(pUmur) // hal ini dapat dilakukan jika menggunakan virtual
+    {
+        cout << "Budi dibuat\n"
+             << endl;
+    }
+};
+
+int main()
+{
+    budi a(12);
+
+    return 0;
+}
